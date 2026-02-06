d2eb359d1a2fe9ff6b0394a6c31a97a98ac60d82
amark2005 <amarkeshav14@gmail.com>
2026-02-06 17:58:19 +0530

    failed too complicaated

--- drag.py
+++ drag.py
@@ -1,5 +1,7 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
+#from matplotlib.animation import FuncAnimation
 
 def dragf(fa, rho, v, cd):
     return 0.5 * rho * (v**2) * fa * cd
@@ -20,11 +22,15 @@ v_kmh_plot = np.arange(0,120, 1)  # 0 to 700 inclusive
 v_ms_plot = v_kmh_plot * (5/18)
 fd_zen_car = dragf(fa, rho, v_ms_plot, cd)
 fd_person=dragf(0.6194,1.168,v_ms_plot,1.15)
-
+for i in range(0,120):
+    v_kmh_plot_for = np.arange(0,i, 1)  # 0 to 700 inclusive
+    v_ms_plot_for = v_kmh_plot_for * (5/18)
+    fd_nigga=dragf(0.6194,1.168,v_ms_plot_for,1.15)
 # Plotting
 plt.figure(figsize=(10, 6))
 plt.plot(fd_zen_car, v_kmh_plot, linewidth=2)
-plt.plot(fd_person, v_kmh_plot, linewidth=2)
+#plt.plot(fd_person, v_kmh_plot, linewidth=2)
+plt.plot(fd_nigga, v_kmh_plot_for, linewidth=2)
 plt.grid(True)
 plt.xlabel("Drag Force (N)")
 plt.ylabel("Speed (km/h)")
